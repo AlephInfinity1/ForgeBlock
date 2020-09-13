39b65359bcecfcad55763767f883004d0e25a3cb
AlephInfinity1 <50240089+AlephInfinity1@users.noreply.github.com>
2020-09-13 22:53:15 +0800

    Added textures for silver fang and 2 lapis pieces (inv only)

---
+++ src/main/java/alephinfinity1/forgeblock/effect/ComboEffect.java
@@ -0,0 +1,12 @@
+package alephinfinity1.forgeblock.effect;
+
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+
+public class ComboEffect extends Effect {
+
+	protected ComboEffect(EffectType typeIn, int liquidColorIn) {
+		super(typeIn, liquidColorIn);
+	}
+
+}
