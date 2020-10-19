c82f553d4cde8b7bf4353060254aebd46f12483f
AlephInfinity1 <50240089+AlephInfinity1@users.noreply.github.com>
2020-10-19 11:52:56 +0800

    Particle changes
    Sorry for frequent commit, just want to send some files between devices

--- src/main/java/alephinfinity1/forgeblock/client/particles/NumericDamageIndicatorParticle.java
+++ src/main/java/alephinfinity1/forgeblock/client/particles/NumericDamageIndicatorParticle.java
@@ -11,8 +11,14 @@ import net.minecraft.client.particle.IParticleRenderType;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.event.EntityViewRenderEvent.CameraSetup;
 
 public class NumericDamageIndicatorParticle extends Particle {
 	
@@ -28,22 +34,49 @@ public class NumericDamageIndicatorParticle extends Particle {
 	@Override
 	public void renderParticle(IVertexBuilder buffer, ActiveRenderInfo renderInfo, float partialTicks) {
 		
-		MatrixStack matrixStack = new MatrixStack();
-		matrixStack.getLast().getMatrix().mul(ForgeBlock.MINECRAFT.gameRenderer.getProjectionMatrix(renderInfo, partialTicks, true));
-		matrixStack.rotate(renderInfo.getRotation());
-		matrixStack.scale(-1.0f, -1.0f, 1.0f);
+		MatrixStack matrixStackIn = new MatrixStack();
+		
+		matrixStackIn.push();
+		ActiveRenderInfo activerenderinfo = ForgeBlock.MINECRAFT.gameRenderer.getActiveRenderInfo();
+		CameraSetup cam = ForgeHooksClient.onCameraSetup(ForgeBlock.MINECRAFT.gameRenderer, activerenderinfo, partialTicks);
+		
+		activerenderinfo.setAnglesInternal(cam.getYaw(), cam.getPitch());
+		
+		//matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(cam.getRoll()));
+		
+		matrixStackIn.rotate(Vector3f.XP.rotationDegrees(activerenderinfo.getPitch()));
+		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(activerenderinfo.getYaw() + 180.0F));
+		
+		matrixStackIn.push();
+		
+		double x = MathHelper.lerp(partialTicks, prevPosX, posX);
+		double y = MathHelper.lerp(partialTicks, prevPosY, posY);
+		double z = MathHelper.lerp(partialTicks, prevPosZ, posZ);
+		
+		matrixStackIn.translate(x - ForgeBlock.MINECRAFT.gameRenderer.getActiveRenderInfo().getProjectedView().getX(), 
+        		y - ForgeBlock.MINECRAFT.gameRenderer.getActiveRenderInfo().getProjectedView().getY(), 
+        		z - ForgeBlock.MINECRAFT.gameRenderer.getActiveRenderInfo().getProjectedView().getZ());
 
 		IRenderTypeBuffer.Impl irendertypebufferimpl = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
-		IVertexBuilder builder = irendertypebufferimpl.getBuffer(RenderType.getSolid());
 		FontRenderer font = Minecraft.getInstance().fontRenderer;
-		font.renderString(message, Minecraft.getInstance().getMainWindow().getScaledWidth() / 2, Minecraft.getInstance().getMainWindow().getScaledHeight() / 2, 0xffffff, false, matrixStack.getLast().getMatrix(), irendertypebufferimpl, false, 0, 15728880);
-		irendertypebufferimpl.finish();
+		EntityRendererManager renderManager = Minecraft.getInstance().getRenderManager();
+		
+		matrixStackIn.push();
+		matrixStackIn.rotate(renderManager.getCameraOrientation());
+		matrixStackIn.scale(-0.025F, -0.025F, 0.025F);
+		Matrix4f matrix4f = matrixStackIn.getLast().getMatrix();
+	    
+		font.renderString(this.message, 0, (float)0, 553648127, false, matrix4f, irendertypebufferimpl, false, 0, 0);
+		
 		//System.out.println("Particle is rendering");
 	}
 	
 	@Override
     public void tick() {
-		
+		age++;
+		if(this.age > this.maxAge) {
+			this.setExpired();
+		}
     }
 
 	@Override
